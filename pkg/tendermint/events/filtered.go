a67ab0ad3d3220c49ed78ba6fe667fed8c23a093
Sammy Liu <liuzhihao1991@gmail.com>
2021-06-03 06:49:44 +0800

    feat: MustSubscribeNewBlockHeader (#6)
    * feat: MustSubscribeNewBlockHeader
    
    * refactor subscriptions
    
    * adding comment
    
    * generalize hub
    
    * add comment
    
    * rename predicate
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>

--- pkg/tendermint/events/filtered.go
+++ pkg/tendermint/events/filtered.go
@@ -2,12 +2,16 @@ package events
 
 import (
 	"fmt"
-	"github.com/axelarnetwork/tm-events/pkg/pubsub"
-	"github.com/axelarnetwork/tm-events/pkg/tendermint/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	tmpubsub "github.com/tendermint/tendermint/libs/pubsub"
 	"github.com/tendermint/tendermint/libs/pubsub/query"
 	tm "github.com/tendermint/tendermint/types"
+
+	"github.com/axelarnetwork/tm-events/pkg/pubsub"
+	"github.com/axelarnetwork/tm-events/pkg/tendermint/types"
 )
 
 // FilteredSubscriber filters events of a subscriber according to a predicate
@@ -41,27 +45,56 @@ func (s FilteredSubscriber) Events() <-chan types.Event {
 	return s.eventChan
 }
 
-// MustSubscribe panics if Subscribe fails
-func MustSubscribe(hub *Hub, eventType string, module string, action string) FilteredSubscriber {
-	subscriber, err := Subscribe(hub, eventType, module, action)
+// Query represents a query used to subscribe a FilteredSubscriber to an event
+type Query struct {
+	TMQuery   *query.Query
+	Predicate func(event types.Event) bool
+}
+
+// MustSubscribeTx panics if subscription to the transaction event fails
+func MustSubscribeTx(pub Publisher, eventType string, module string, action string) FilteredSubscriber {
+	q := Query{
+		TMQuery: query.MustParse(fmt.Sprintf("%s='%s' AND %s.%s='%s'",
+			tm.EventTypeKey, tm.EventTx, eventType, sdk.AttributeKeyModule, module)),
+		Predicate: func(e types.Event) bool {
+			return e.Type == eventType && e.Module == module && e.Action == action
+		},
+	}
+	return MustSubscribe(pub, q,
+		func(err error) error {
+			return sdkerrors.Wrapf(err, "subscription to event {type %s, module %s, action %s} failed", eventType, module, action)
+		})
+}
+
+// MustSubscribeNewBlockHeader panics if subscription to the block header event fails
+func MustSubscribeNewBlockHeader(pub Publisher) FilteredSubscriber {
+	q := Query{
+		TMQuery:   query.MustParse(fmt.Sprintf("%s='%s'", tm.EventTypeKey, tm.EventNewBlockHeader)),
+		Predicate: func(e types.Event) bool { return e.Type == minttypes.EventTypeMint },
+	}
+	return MustSubscribe(pub, q,
+		func(err error) error { return sdkerrors.Wrapf(err, "subscription to block header failed") })
+}
+
+// MustSubscribe panics if the subscription to the given query fails
+func MustSubscribe(pub Publisher, query Query, onFail func(error) error) FilteredSubscriber {
+	subscriber, err := Subscribe(pub, query)
 	if err != nil {
-		panic(sdkerrors.Wrapf(err, "subscription to event {type %s, module %s, action %s} failed", eventType, module, action))
+		panic(onFail(err))
 	}
 	return subscriber
 }
 
 // Subscribe returns a filtered subscriber that only streams events of the given type, module and action
-func Subscribe(hub *Hub, eventType string, module string, action string) (FilteredSubscriber, error) {
-	qString := fmt.Sprintf("%s='%s' AND %s.%s='%s'",
-		tm.EventTypeKey, tm.EventTx, eventType, sdk.AttributeKeyModule, module)
-	subscriber, err := hub.Subscribe(query.MustParse(qString))
+func Subscribe(pub Publisher, q Query) (FilteredSubscriber, error) {
+	subscriber, err := pub.Subscribe(q.TMQuery)
 	if err != nil {
 		return FilteredSubscriber{}, err
 	}
-	return NewFilteredSubscriber(
-		subscriber,
-		func(e types.Event) bool {
-			return e.Type == eventType && e.Module == module && e.Action == action
-		},
-	), nil
+	return NewFilteredSubscriber(subscriber, q.Predicate), nil
+}
+
+// Publisher can create a subscription for the given query
+type Publisher interface {
+	Subscribe(tmpubsub.Query) (pubsub.Subscriber, error)
 }
