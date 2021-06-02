d8ed0f74baac0f0005e2af654c335845393cef98
Christian Gorenflo <christian@axelar.network>
2021-06-02 16:54:03 -0400

    refactor subscriptions

--- pkg/tendermint/events/filtered.go
+++ pkg/tendermint/events/filtered.go
@@ -3,13 +3,14 @@ package events
 import (
 	"fmt"
 
-	"github.com/axelarnetwork/tm-events/pkg/pubsub"
-	"github.com/axelarnetwork/tm-events/pkg/tendermint/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	"github.com/tendermint/tendermint/libs/pubsub/query"
 	tm "github.com/tendermint/tendermint/types"
+
+	"github.com/axelarnetwork/tm-events/pkg/pubsub"
+	"github.com/axelarnetwork/tm-events/pkg/tendermint/types"
 )
 
 // FilteredSubscriber filters events of a subscriber according to a predicate
@@ -43,44 +44,50 @@ func (s FilteredSubscriber) Events() <-chan types.Event {
 	return s.eventChan
 }
 
-// MustSubscribe panics if Subscribe fails
-func MustSubscribe(hub *Hub, eventType string, module string, action string) FilteredSubscriber {
-	subscriber, err := Subscribe(hub, eventType, module, action)
+type Query struct {
+	TMQuery           *query.Query
+	DetailedPredicate func(event types.Event) bool
+}
+
+// MustSubscribeTx panics if subscription to the transaction event fails
+func MustSubscribeTx(hub *Hub, eventType string, module string, action string) FilteredSubscriber {
+	q := Query{
+		TMQuery: query.MustParse(fmt.Sprintf("%s='%s' AND %s.%s='%s'",
+			tm.EventTypeKey, tm.EventTx, eventType, sdk.AttributeKeyModule, module)),
+		DetailedPredicate: func(e types.Event) bool {
+			return e.Type == eventType && e.Module == module && e.Action == action
+		},
+	}
+	return MustSubscribe(hub, q,
+		func(err error) error {
+			return sdkerrors.Wrapf(err, "subscription to event {type %s, module %s, action %s} failed", eventType, module, action)
+		})
+}
+
+// MustSubscribeNewBlockHeader panics if subscription to the block header event fails
+func MustSubscribeNewBlockHeader(hub *Hub) FilteredSubscriber {
+	q := Query{
+		TMQuery:           query.MustParse(fmt.Sprintf("%s='%s'", tm.EventTypeKey, tm.EventNewBlockHeader)),
+		DetailedPredicate: func(e types.Event) bool { return e.Type == minttypes.EventTypeMint },
+	}
+	return MustSubscribe(hub, q,
+		func(err error) error { return sdkerrors.Wrapf(err, "subscription to block header failed") })
+}
+
+// MustSubscribe panics if the subscription to the given query fails
+func MustSubscribe(hub *Hub, query Query, onFail func(error) error) FilteredSubscriber {
+	subscriber, err := Subscribe(hub, query)
 	if err != nil {
-		panic(sdkerrors.Wrapf(err, "subscription to event {type %s, module %s, action %s} failed", eventType, module, action))
+		panic(onFail(err))
 	}
 	return subscriber
 }
 
-func MustSubscribeNewBlockHeader(hub *Hub) FilteredSubscriber {
-	qString := fmt.Sprintf("%s='%s'",
-		tm.EventTypeKey, tm.EventNewBlockHeader)
-
-	subscriber, err := hub.Subscribe(query.MustParse(qString))
-	if err != nil {
-		panic(sdkerrors.Wrapf(err, "subscription to block header failed"))
-	}
-
-	return NewFilteredSubscriber(
-		subscriber,
-		func(e types.Event) bool {
-			return e.Type == minttypes.EventTypeMint
-		},
-	)
-}
-
 // Subscribe returns a filtered subscriber that only streams events of the given type, module and action
-func Subscribe(hub *Hub, eventType string, module string, action string) (FilteredSubscriber, error) {
-	qString := fmt.Sprintf("%s='%s' AND %s.%s='%s'",
-		tm.EventTypeKey, tm.EventTx, eventType, sdk.AttributeKeyModule, module)
-	subscriber, err := hub.Subscribe(query.MustParse(qString))
+func Subscribe(hub *Hub, q Query) (FilteredSubscriber, error) {
+	subscriber, err := hub.Subscribe(q.TMQuery)
 	if err != nil {
 		return FilteredSubscriber{}, err
 	}
-	return NewFilteredSubscriber(
-		subscriber,
-		func(e types.Event) bool {
-			return e.Type == eventType && e.Module == module && e.Action == action
-		},
-	), nil
+	return NewFilteredSubscriber(subscriber, q.DetailedPredicate), nil
 }
